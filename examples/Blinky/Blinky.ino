ea6031d307e343d1fcf100a618244597c3c63575
Caleb H <147654545+chusovich@users.noreply.github.com>
2024-03-13 15:57:37 -0400

    Update Blinky.ino

--- examples/Blinky/Blinky.ino
+++ examples/Blinky/Blinky.ino
@@ -5,8 +5,8 @@
   The tasks are paused and resumes after different amounts of time and eventually the tasks are deleted
 */
 
-Task slowBlinkyTask("Slow Blinky",1024,1); // name, task stack size in bytes, priority
-Task fastBlinkyTask("Fast Blinky",1024,1);
+Task slowBlinkTask("Slow Blinky",1024,1); // name, task stack size in bytes, priority
+Task fastBlinkTask("Fast Blinky",1024,1);
 
 #define LED1 1 // slow led pin
 #define LED2 2 // fast led pin
@@ -31,19 +31,19 @@ void blinkFast(void *) {
 void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
-  slowBlinkyTask.create(blinkSlow); // start the task
-  fastBlinkyTask.create(blinkFast);
+  slowBlinkTask.create(blinkSlow); // start the task
+  fastBlinkTask.create(blinkFast);
 }
 
 void loop() {
   vTaskDelay(10000);
-  slowBlinkyTask.suspend(); // stop the slow blinky task after 10 sec
+  slowBlinkTask.suspend(); // stop the slow blinky task after 10 sec
   vTaskDelay(5000);
-  slowBlinkyTask.resume(); // start blinking slowly again
-  fastBlinkyTask.suspend(); // stop the fast blinky task
+  slowBlinkTask.resume(); // start blinking slowly again
+  fastBlinkTask.suspend(); // stop the fast blinky task
   vTaskDelay(5000);
   slowBlinkTask.destroy(); // delete the slow blinkly task
-  fastBlinklyTask.resume(); // start fast blinky task
+  fastBlinkTask.resume(); // start fast blinky task
   vTaskDelay(2000);
-  fastBlinkyTask.destroy(); // stop the fast blinky task
+  fastBlinkTask.destroy(); // stop the fast blinky task
 }
